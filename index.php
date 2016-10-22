ab24dc7197c24b9344099d2ebfd032cfe7d276c0
Matthew Jennings <matthewadjennings@gmail.com>
2016-10-22 18:38:23 +0100

    index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login Page Test</title>
+  </head>
+  <body>
+    <form action="index.php" method="post">
+      <input type="text" name="usrid" placeholder="Username">
+      <input type="text" name="pword" placeholder="Password">
+      <input type="submit" name="sub" placeholder="Submit">
+
+    </form>
+  </body>
+</html>
